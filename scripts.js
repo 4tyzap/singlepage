1b158be4beefa7afb307896e39c9ba89e4b65d98
4tyzap <123526259+4tyzap@users.noreply.github.com>
2025-01-16 15:57:09 +0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,15 +2,28 @@ function toggleImage(element) {
     const img = element.querySelector('img');
     const fullSrc = img.getAttribute('data-full');
     const overlay = document.getElementById('overlay');
+    const productFull = document.createElement('div');
+    productFull.classList.add('product-full');
+    productFull.innerHTML = `<img src="${fullSrc}" alt="Full Image">`;
 
-    overlay.innerHTML = `<img src="${fullSrc}" alt="Full Image">`;
+    overlay.appendChild(productFull);
     overlay.classList.add('active');
+
+    setTimeout(() => {
+        productFull.classList.add('active');
+    }, 0);
 }
 
 function closeImage() {
     const overlay = document.getElementById('overlay');
-    overlay.classList.remove('active');
-    overlay.innerHTML = '';
+    const productFull = overlay.querySelector('.product-full');
+
+    productFull.classList.remove('active');
+
+    setTimeout(() => {
+        overlay.classList.remove('active');
+        overlay.innerHTML = '';
+    }, 500); // Время анимации
 }
 
 document.getElementById('contact-form').addEventListener('submit', function(event) {
